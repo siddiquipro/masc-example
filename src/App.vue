0b2819a9fff89ee8972eebdbcdd2100f54773dea
mohammadsiddiqui <masiddiqui91@gmail.com>
2023-06-16 14:44:14 +0100

    scard test

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,26 @@
     <SCard title="This is card data">
       <div>
         <p class="pb-4">Hello</p>
-        <SBtn class="px-10 btn-sm btn-primary rounded" @click="onClick">Test Notify</SBtn>
+        <SBtn class="px-10 btn-sm btn-primary rounded" @click="onClick"
+          >Test Notify</SBtn
+        >
       </div>
     </SCard>
+
+    <SForms />
   </div>
 </template>
 
 <script setup lang="ts">
-import { SBtn } from "masc-vue";
-import { SCard, useNotify } from "masc-vue";
-
+import { SBtn } from 'masc-vue';
+import { SCard, useNotify } from 'masc-vue';
+import SForms from './components/SForms.vue';
 const notify = useNotify();
 
 async function onClick() {
-  const yes = await notify.confirm("Please confirm if you would like to do this?");
+  const yes = await notify.confirm(
+    'Please confirm if you would like to do this?'
+  );
   if (yes) {
     notify.success(`The response is confirmed`);
   } else {
---
+++ src/components/SForms.vue
@@ -0,0 +1,108 @@
+<template>
+  <SCard title="Form">
+    <div v-for="col in config.columns">
+      <SFormitem
+        :label="col.label"
+        :type="col.type"
+        :options="opts"
+        v-model="ds[col.field]"
+      />
+    </div>
+  </SCard>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue';
+import { SCard, SFormitem } from 'masc-vue';
+
+const ds: any = reactive({ obj: {} });
+
+const opts: any[] = [];
+for (let i = 1; i <= 10; i++) {
+  opts.push({ value: `Value ${i}`, text: `Text ${i}` });
+}
+
+const config = {
+  columns: [
+    {
+      field: 'texta',
+      label: 'This is label for type TEXT',
+      type: 'text',
+      info: '',
+      errorMsg: '',
+      validation: '',
+      classes: '',
+    },
+    {
+      field: 'search',
+      label: 'This is label for type SEARCH',
+      type: 'search',
+      info: '',
+      errorMsg: '',
+      validation: '',
+      classes: '',
+    },
+    {
+      field: 'textareab',
+      label: 'This is label for type TEXTAREA',
+      type: 'textarea',
+      info: '',
+      errorMsg: '',
+      validation: '',
+      classes: '',
+    },
+    {
+      field: 'radioc',
+      label: 'This is label for type RADIO',
+      type: 'radio',
+      info: '',
+      errorMsg: '',
+      validation: '',
+      classes: '',
+    },
+    {
+      field: 'dated',
+      label: 'This is label for type DATE',
+      type: 'date',
+      info: '',
+      errorMsg: '',
+      validation: '',
+      classes: '',
+    },
+    {
+      field: 'boolean',
+      label: 'This is label for type BOOLEAN',
+      type: 'boolean',
+      info: '',
+      errorMsg: '',
+      validation: '',
+      classes: '',
+    },
+    {
+      field: 'checkbox',
+      label: 'This is label for type CHECKBOX',
+      type: 'checkbox',
+      info: '',
+      errorMsg: '',
+      validation: '',
+      classes: '',
+    },
+    {
+      field: 'select',
+      label: 'This is label for type SELECT',
+      type: 'select',
+      info: '',
+      errorMsg: '',
+      validation: '',
+      classes: '',
+    },
+  ],
+};
+
+console.log(config);
+
+// ordering of cols
+// horizontal
+// vertical
+// grouped
+</script>
