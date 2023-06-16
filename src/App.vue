80c94648cd1503d661b7582794bab2ef6b878036
Mohammad Siddiqui <masiddiqui91@gmail.com>
2023-06-16 14:23:19 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-6 flex flex-col gap-6">
+    <div class="text-xl font-semibold">Hello World</div>
+
+    <SCard title="This is card data">
+      <div>
+        <p class="pb-4">Hello</p>
+        <SBtn class="px-10 btn-sm btn-primary rounded" @click="onClick">Test Notify</SBtn>
+      </div>
+    </SCard>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { SBtn } from "masc-vue";
+import { SCard, useNotify } from "masc-vue";
+
+const notify = useNotify();
+
+async function onClick() {
+  const yes = await notify.confirm("Please confirm if you would like to do this?");
+  if (yes) {
+    notify.success(`The response is confirmed`);
+  } else {
+    notify.warning(`User did not like it`);
+  }
+}
+</script>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "masc-vue/dist/style.css";
+
+const app = createApp(App);
+
+app.mount("#app");
